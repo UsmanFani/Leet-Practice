a4da139d314e545a2ef922989080efefc9604385
UsmanFani <usmanfani86@gmail.com>
2022-06-18 01:03:32 +0530

    Leet Practice

--- src/main/kotlin/easy/SortedSquares.kt
+++ src/main/kotlin/easy/SortedSquares.kt
@@ -13,16 +13,24 @@ class SortedSquares {
     //Input: nums = [-7,-3,2,3,11]
     //Output: [4,9,9,49,121]
     fun sortedSquares(nums:IntArray):IntArray{
-        for(i in nums.indices){
-            nums[i] = nums[i]*nums[i]
+        var arr = IntArray(nums.size)
+        var start = 0
+        var end = nums.lastIndex
+        for(i in nums.lastIndex downTo 0){
+            if(nums[start]>=0){
+                arr[i] = nums[end]*nums[end]
+                start++
+                end--
+            }else{
+                if(nums[start]*-1>nums[end]){
+                    arr[i] = nums[start]*nums[start]
+                    start++
+                }else{
+                    arr[i] = nums[end]*nums[end]
+                    end--
+                }
+            }
         }
-       for(i in 0 until nums.lastIndex){
-           if(nums[i]>nums[i+1]){
-               val temp = nums[i+1]
-               nums[i+1] = nums[i]
-               nums[i] = temp
-           }
-       }
-        return nums
+        return arr
     }
 }
