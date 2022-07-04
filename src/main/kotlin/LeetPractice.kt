3cd614246b0ec52067c6186a900c9c25bf42cf85
UsmanFani <usmanfani86@gmail.com>
2022-07-05 00:57:08 +0530

    Leet Practice

--- src/main/kotlin/LeetPractice.kt
+++ src/main/kotlin/LeetPractice.kt
@@ -3,7 +3,8 @@ import easy.*
 import medium.*
 
 fun main() {
-    val arr = arrayOf(intArrayOf(2,3), intArrayOf(5, 5), intArrayOf(2,2), intArrayOf(3,4), intArrayOf(3,4))
-    val merge = MergeIntervals().mergeIntervals(arr)
-    merge.forEach { println(it.asList()) }
+    var nums = intArrayOf(1,2,3)
+    var k = 3
+    val subArray = SubArraySum().optimSubArray(nums,k)
+    print(subArray)
 }
