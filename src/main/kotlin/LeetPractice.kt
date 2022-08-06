48cebe0e6d1d5dc200255e56ce6322e24c11d17d
UsmanFani <usmanfani86@gmail.com>
2022-08-07 00:59:11 +0530

    Leet Practice

--- src/main/kotlin/LeetPractice.kt
+++ src/main/kotlin/LeetPractice.kt
@@ -3,7 +3,8 @@ import easy.*
 import medium.*
 
 fun main() {
-   var nums = intArrayOf(3,1,3,4,2)
-    var duplicate = FindDuplicate().optimFindDuplicate(nums)
-    println(duplicate)
+   var nums = intArrayOf(2,3,6,7)
+    var target = 7
+    var combination = CombinationSum().combinationSum(nums,target)
+    println(combination)
 }
