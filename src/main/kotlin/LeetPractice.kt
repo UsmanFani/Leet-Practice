bfb25bfaf0d362c1be89f9eb5541b7c647f78dbd
UsmanFani <usmanfani86@gmail.com>
2022-09-05 21:51:59 +0530

    Leet Practice

--- src/main/kotlin/LeetPractice.kt
+++ src/main/kotlin/LeetPractice.kt
@@ -3,10 +3,27 @@ import dp.KnapsackProblem
 import easy.*
 import hard.FirstMissingPositive
 import hard.LargestRectangleInHistogram
+import hard.RandomizedCollection
 import medium.*
 
 fun main() {
-    var nums = intArrayOf(2,0)
-    var num = LargestRectangleInHistogram().optimLargestResctangleArea(nums)
-    println(num)
+   var randomize = RandomizedCollection()
+    println(randomize.insert(1))
+    println(randomize.insert(1))
+    println(randomize.insert(2))
+    println(randomize.insert(1))
+    println(randomize.insert(2))
+    println(randomize.insert(2))
+
+    println(randomize.remove(1))
+    println(randomize.remove(2))
+    println(randomize.remove(2))
+    println(randomize.remove(2))
+
+    println(randomize.getRandom())
+    println(randomize.getRandom())
+    println(randomize.getRandom())
+    println(randomize.getRandom())
+    println(randomize.getRandom())
+
 }
---
+++ src/main/kotlin/ProblemOfTheDay/NumberWithSameConsecutiveDiff.kt
@@ -0,0 +1,17 @@
+package ProblemOfTheDay
+
+class NumberWithSameConsecutiveDiff {
+    //Example 1:
+    //Input: n = 3, k = 7
+    //Output: [181,292,707,818,929]
+    //Explanation: Note that 070 is not a valid number, because it has leading zeroes.
+
+    //Example 2:
+    //Input: n = 2, k = 1
+    //Output: [10,12,21,23,32,34,43,45,54,56,65,67,76,78,87,89,98]
+    fun numsSameConsecDiff(n:Int,k:Int):IntArray{
+        var result = intArrayOf()
+
+        return result
+    }
+}
