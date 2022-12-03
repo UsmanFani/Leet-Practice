f2b617f67cb676cc3789c2a01a68c9e2dea5dd82
UsmanFani <usmanfani86@gmail.com>
2022-12-03 22:39:04 +0530

    Leet Practice

--- src/main/kotlin/LeetPractice.kt
+++ src/main/kotlin/LeetPractice.kt
@@ -1,11 +1,12 @@
 import Strings.Easy.AddStrings
+import Strings.Easy.ValidPalindrome2
+import dp.medium.PalindromicSubstring
 
 fun main() {
 
 
-    var num1 = "11"
-    var num2 = "123"
-    var sum = AddStrings().addStrings(num1, num2)
-    println(sum)
+    var str = "aabbbsa"
+    var pal = ValidPalindrome2().validPalindrome(str)
+    println(pal)
 }
 
--- src/main/kotlin/Strings/Easy/ValidPalindrome2.kt
+++ src/main/kotlin/Strings/Easy/ValidPalindrome2.kt
@@ -11,6 +11,26 @@ class ValidPalindrome2 {
     //Output: true
     //Explanation: You could delete the character 'c'.
     fun validPalindrome(s: String): Boolean {
+        var start = 0
+        var end = s.lastIndex
+        while (start < end) {
+            if (s[start] != s[end]) {
+                return check(start + 1, end, s) || check(start, end - 1, s)
+            }
+            start++
+            end--
+        }
+        return true
+    }
 
+    private fun check(start: Int, end: Int, s: String): Boolean {
+        var start = start
+        var end = end
+        while (start < end) {
+            if (s[start] != s[end]) return false
+            start++
+            end--
+        }
+        return true
     }
 }
