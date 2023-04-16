82b34ab249c443ae60df48f616ec9041462cb5b0
UsmanFani <usmanfani86@gmail.com>
2023-04-17 01:50:09 +0530

    Leet Practice

--- src/main/kotlin/LeetPractice.kt
+++ src/main/kotlin/LeetPractice.kt
@@ -2,6 +2,7 @@ import ProblemOfTheDay.MinimumAverageDifference
 import Strings.Easy.AddStrings
 import Strings.Easy.RomanToInt
 import Strings.Easy.ValidPalindrome2
+import Strings.Medium.GroupAnagram
 import Strings.Medium.LongestPalindromicSubstring
 import Strings.Medium.MinimumRemoveToMakeValidParantheses
 import dp.medium.PalindromicSubstring
@@ -9,8 +10,9 @@ import dp.medium.PalindromicSubstring
 fun main() {
 
 
-    var str = "cbbd"
-    var pal = LongestPalindromicSubstring().longestPalindrome(str)
-    println(pal)
+    var str = arrayOf("")
+    val anagram = GroupAnagram()
+   val res =  anagram.groupAnagram(str)
+    println(res)
 }
 
--- src/main/kotlin/Strings/Medium/GroupAnagram.kt
+++ src/main/kotlin/Strings/Medium/GroupAnagram.kt
@@ -8,12 +8,44 @@ class GroupAnagram {
 
     //Example 2:
     //Input: strs = [""]
+
     //Output: [[""]]
 
     //Example 3:
     //Input: strs = ["a"]
     //Output: [["a"]]
-    fun groupAnagram(strs:Array<String>):List<List<String>>{
-
+    fun groupAnagram(strs: Array<String>): List<List<String>> {
+        val charCountList = mutableListOf<Map<Char, Int>>()
+        val visitedMap = mutableSetOf<Int>()
+        val anagramList = mutableListOf<List<String>>()
+        strs.forEach {
+            charCountList.add(countMap(it))
+        }
+        charCountList.forEachIndexed { index, map ->
+            if (!visitedMap.contains(index)) {
+                val list = mutableListOf<String>(strs[index])
+                for (i in index + 1..charCountList.lastIndex) {
+                    if (visitedMap.contains(i)) continue
+                    if (map.equals(charCountList[i])) {
+                        list.add(strs[i])
+                        visitedMap.add(i)
+                    }
+                }
+                if (list.isNotEmpty()) anagramList.add(list)
+            }
+        }
+        return anagramList
     }
+
+    private fun countMap(s: String): Map<Char, Int> {
+        val charMap = mutableMapOf<Char, Int>()
+        s.forEach {
+            if (charMap.containsKey(it)) {
+                charMap[it] = charMap[it]!! + 1
+            } else charMap[it] = 1
+        }
+        return charMap
+    }
+
 }
+
